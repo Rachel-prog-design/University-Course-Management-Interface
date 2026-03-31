78c2184873e48d5958eb41dd73221339a726b155
Rachel-prog-design <rachelnyinawabana7@gmail.com>
2026-03-31 16:01:04 +0200

    added login.jsx

---
+++ course-management/src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import API from "../api/api";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await API.post("/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+      alert("Login successful!");
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Login failed!");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h2>Supervisor Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
